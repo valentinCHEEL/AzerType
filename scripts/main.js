414334706201727c5488013bb113faabea3edba8
Valentin Cheel <valentin.cheel.pro@gmail.com>
2024-09-03 11:10:55 +0200

    ajout du css

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let divZoneProposition = document.querySelector(".zoneProposition")
+console.log(divZoneProposition)
+
+let spanScore = document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)
 //On apelle la fonction pour lancer le jeu
-lancerJeu()
+//lancerJeu()
